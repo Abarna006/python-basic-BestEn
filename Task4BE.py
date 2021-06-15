d6a37fb23ef36a8d8a6d317f2b86e64c585f2fe2
Abarna <84118972+Abarna006@users.noreply.github.com>
2021-06-15 12:08:52 +0530

    Add files via upload

---
+++ Task4BE.py
@@ -0,0 +1,18 @@
+a = 2
+b = 4
+c = 6
+div = a/10
+mul = b*50
+ad = c+60
+print(div)
+print(mul)
+print(ad)
+name = "swwagg"
+x = name.replace('a','g')
+print(x)
+a = 15
+b = 18.2
+x = float(a)
+y = int(b)
+print(x)
+print(y)
